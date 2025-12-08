82d6b3e79f769d6d479bee8f44cf0e1effa8f2e3
sevaishere <vsevolod.kurteev@gmail.com>
2025-12-08 09:41:18 +0400

    Add files via upload

--- task2/task2.py
+++ task2/task2.py
@@ -2,6 +2,15 @@
 
 import sys
 
+def args_check(numb_of_args: int, args_rules: str):
+    args_len = len(sys.argv[1:])
+    if args_len < 1:
+        sys.exit('Не введено ни одного аргумента.\n' + args_rules)
+    elif args_len < numb_of_args:
+        sys.exit('Введено слишком мало аргументов.\n' + args_rules)
+    elif args_len > numb_of_args:
+        sys.exit('Введено слишком много аргументов.\n' + args_rules)
+
 def ellipse_position(centre, radius, x, y):
     centre_x = centre[0]
     centre_y = centre[1]
@@ -17,8 +26,12 @@ def ellipse_position(centre, radius, x, y):
     else:
         return 2
 
-file1_path = input('Введите путь к 1-му файлу: ')
-file2_path = input('Введите путь ко 2-му файлу: ')
+args_check(2, 'Необходимо ввести 2 аргумента: '
+              'сначала путь к файлу с параметрами эллипса, '
+              'а затем путь к файлу с координатами точек.')
+
+file1_path = sys.argv[1]
+file2_path = sys.argv[2]
 
 ellipse_par = []
 with open(file1_path, 'r') as file:
@@ -30,12 +43,15 @@ points = []
 counter = 0
 with open(file2_path, 'r') as file:
     for line in file:
-        data = [int(x) for x in line.split()]
-        points.append(data)
-        counter += 1
-        if counter > 100:
-            sys.exit('Во 2-ом файле количество пар координат превышает 100')
+        stripped_line = line.strip()
+        if stripped_line:
+            data = [int(x) for x in line.split()]
+            points.append(data)
+            counter += 1
+            if counter > 100:
+                sys.exit('В файле с координатами точек количество пар координат превышает 100')
+        else:
+            pass
 
 for point in points:
     print(ellipse_position(ellipse_par[0], ellipse_par[1], point[0], point[1]), end='\n')
-
