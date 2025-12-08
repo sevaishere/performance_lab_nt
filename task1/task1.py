62edb071c43eb8af43900d583cc64d17d94239bb
sevaishere <vsevolod.kurteev@gmail.com>
2025-12-08 09:40:52 +0400

    Add files via upload

--- task1/task1.py
+++ task1/task1.py
@@ -1,5 +1,16 @@
 # Задание 1
 
+import sys
+
+def args_check(numb_of_args: int, args_rules: str):
+    args_len = len(sys.argv[1:])
+    if args_len < 1:
+        sys.exit('Не введено ни одного аргумента.\n' + args_rules)
+    elif args_len < numb_of_args:
+        sys.exit('Введено слишком мало аргументов.\n' + args_rules)
+    elif args_len > numb_of_args:
+        sys.exit('Введено слишком много аргументов.\n' + args_rules)
+
 def circular_array(n, m):
     num = 1
     arr = []
@@ -13,10 +24,14 @@ def circular_array(n, m):
         path = path + str(i)
     return path
 
-n1 = int(input('Введите параметр «n» для 1-го массива: '))
-m1 = int(input('Введите параметр «m» для 1-го массива: '))
-n2 = int(input('Введите параметр «n» для 2-го массива: '))
-m2 = int(input('Введите параметр «m» для 2-го массива: '))
+args_check(4, 'Необходимо ввести 4 аргумента: '
+              'параметры n и m сначала для первого массива, '
+              'а затем для второго.')
+
+n1 = int(sys.argv[1])
+m1 = int(sys.argv[2])
+n2 = int(sys.argv[3])
+m2 = int(sys.argv[4])
 
 output = circular_array(n1, m1) + circular_array(n2, m2)
 
