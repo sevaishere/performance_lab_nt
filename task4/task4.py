6406d56e2032619b16ceab0f4ef19d490d3a2a3d
sevaishere <vsevolod.kurteev@gmail.com>
2025-12-08 09:41:50 +0400

    Add files via upload

--- task4/task4.py
+++ task4/task4.py
@@ -1,6 +1,15 @@
 # Задание 4
 
-import statistics
+import sys, statistics
+
+def args_check(numb_of_args: int, args_rules: str):
+    args_len = len(sys.argv[1:])
+    if args_len < 1:
+        sys.exit('Не введено ни одного аргумента.\n' + args_rules)
+    elif args_len < numb_of_args:
+        sys.exit('Введено слишком мало аргументов.\n' + args_rules)
+    elif args_len > numb_of_args:
+        sys.exit('Введено слишком много аргументов.\n' + args_rules)
 
 def moves_to_equal(nums):
     median = int(statistics.median(nums))
@@ -10,9 +19,11 @@ def moves_to_equal(nums):
     if moves <= 20:
         return moves
     else:
-        return '20-ти ходов недостаточно для приведения всех элементов массива к одному числу'
+        return '20-ти ходов недостаточно для приведения всех элементов массива к одному числу.'
 
-file_path = input('Введите путь к файлу: ')
+args_check(1, 'В качестве аргумента необходимо ввести путь к файлу с массивом.')
+
+file_path = sys.argv[1]
 
 nums = []
 with open(file_path, 'r') as file:
